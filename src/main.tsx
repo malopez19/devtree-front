21dd8682da40e1bd479182e4a5f48e352a54d82f
malopez19 <lopezmateojurado@gmail.com>
2026-06-16 20:11:49 -0500

    feat: add React Query for data fetching and implement Profile form with input fields

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import './index.css'
 import AppRouter from './router'
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppRouter />
+    <QueryClientProvider client={queryClient}>
+      <AppRouter />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </StrictMode>,
 )
